848730a1eae631a5016949dd1f788e201ae6754a
Dinis Cruz <dinis.cruz@owasp.org>
2024-06-08 00:46:33 +0100

    added requirements.txt to render example

--- user_deployment_examples/providers/render/cbr_website__app.py
+++ user_deployment_examples/providers/render/cbr_website__app.py
@@ -1,4 +1,10 @@
-from cbr_website_beta.flask.Flask_Site import Flask_Site
 
-flask_site = Flask_Site()
-app        = flask_site.app()
+def main():
+    from cbr_website_beta.flask.Flask_Site import Flask_Site
+
+    flask_site = Flask_Site()
+    app = flask_site.app()
+    app.run()
+
+if __name__ == "__main__":
+    main()
